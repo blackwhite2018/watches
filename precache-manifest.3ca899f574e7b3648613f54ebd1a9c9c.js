fb0962da57a68ac692c759c362abaa0581c65faa
blackwhite2018 <39052924+blackwhite2018@users.noreply.github.com>
2020-06-20 00:24:35 +0300

    Updates

---
+++ precache-manifest.3ca899f574e7b3648613f54ebd1a9c9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83dcf16834d851df29cde0a190f9b9e",
+    "url": "/watches/index.html"
+  },
+  {
+    "revision": "e8bf5deac72b6c599226",
+    "url": "/watches/static/js/2.24bb451e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/watches/static/js/2.24bb451e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc61f1d670a336aea92",
+    "url": "/watches/static/js/main.ad0c7e6b.chunk.js"
+  },
+  {
+    "revision": "8e5ea52d421d3805e323",
+    "url": "/watches/static/js/runtime-main.97d5d9a7.js"
+  }
+]);
